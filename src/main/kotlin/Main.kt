8ed922ca1556ba355a16b3a973263654992f1625
Klein254 <rayobiero62@gmail.com>
2023-03-14 12:37:22 +0300

    my first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var num1 = 30
+    var num2 = 100
+    var result = num2 + num1
+    print(result)
+}
---
+++ src/main/kotlin/var.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var num1 = 68
+    var num2 = 1.7
+    var bmi = num1 / (num2*num2)
+    print(bmi)
+
+}
