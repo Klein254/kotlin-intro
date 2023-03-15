90877cb1980d6b38892accf1d4b52361bff81679
Klein254 <rayobiero62@gmail.com>
2023-03-15 11:40:50 +0300

    datatypes in kotlin

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    //Integer or Int
+    var age:Int = 20
+    var age= 30
+
+    //Strings
+    var name = "Hans"
+
+    //Double
+    var temperature:Double = 36.51
+
+    //Boolean (true or false)
+    var status = false
+
+    //Float
+    var point:Float = 8.5F
+
+    //Convert Double to float
+    var marks = 46.587F
+
+
+}
--- src/main/kotlin/var.kt
+++ src/main/kotlin/var.kt
@@ -3,5 +3,4 @@ fun main(args: Array<String>) {
     var num2 = 1.7
     var bmi = num1 / (num2*num2)
     print(bmi)
-
 }
