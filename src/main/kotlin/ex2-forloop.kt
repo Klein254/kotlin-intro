d41f76b24e3276191ad500f6d5cdda9384f0dbca
Klein254 <rayobiero62@gmail.com>
2023-03-16 13:03:52 +0300

    fizzbuzz

---
+++ src/main/kotlin/ex2-forloop.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    for(i in 1..100){
+        if (i % 15 == 0){
+            println("$i Fizz")
+        }
+        else if (i % 5 == 0){
+            println("$i buzz")
+        }
+        else if (i % 3 == 0){
+            println("$i Fizzbuzz")
+        }
+    }
+
+}
