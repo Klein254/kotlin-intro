26e054d4dde9f18e19c18c5237942fb4ee30a2d4
Klein254 <rayobiero62@gmail.com>
2023-03-16 11:48:33 +0300

    invalid edit

--- src/main/kotlin/compare-grade.kt
+++ src/main/kotlin/compare-grade.kt
@@ -3,28 +3,31 @@ fun main(args: Array<String>) {
 
     var marks:Int = readLine()!!.toInt()
 
-    if (marks > 79){
+    if (marks > 79 && marks <= 100){
         print("Your grade is A")
         print(" ")
         print("Congratulations")
     }
-    else if (marks > 69){
+    else if (marks > 69 && marks < 80){
         print("Your grade is A-")
 
     }
-    else if (marks > 59){
+    else if (marks > 59 && marks < 70){
         print("Your grade is B")
     }
-    else if (marks >49){
+    else if (marks >49 && marks < 60){
         print("Your grade is C")
     }
-    else if (marks >39){
+    else if (marks >39 && marks < 50){
         print("Your grade is D")
     }
-    else {
+    else if (marks <40 && marks >= 0){
         print("Your grade is E")
         print(" ")
         print("You Have to retake the exam")
+    }
+    else {
+        print("Invalid Marks")
     }
 
 }
